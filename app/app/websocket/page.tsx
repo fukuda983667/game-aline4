8ce5e8e715f4b8071b63b26bf17484c410f8abad
fukuda983667 <sfu983667@gmail.com>
2025-10-12 21:26:59 +0900

    pusherテスト用ページ

--- app/app/websocket/page.tsx
+++ app/app/websocket/page.tsx
@@ -1,24 +1,24 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 import React from 'react';
-
-const PUSHER_KEY = '49d48050fbe3aad25b13';
 
 export default function Test() {
     const [data, setData] = useState('');
 
     useEffect(() => {
         Pusher.logToConsole = true;
-        const pusher = new Pusher(PUSHER_KEY, {
-            cluster: 'ap3',
+
+        const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+            cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string
         });
+
 
         const channel = pusher.subscribe('hello');
         channel.bind('HelloWorld', (data: any) => {
             console.log('Received:', data);
-            setData(data.message); // ← 修正
+            setData(data.message);
         });
 
         return () => {
